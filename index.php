7ec988f48fd91f7fe4575579a3e0dcaf4e52c9f4
Rafufu20 <150641074+Rafufu20@users.noreply.github.com>
2024-11-09 17:24:45 +0800

    dada

--- index.php
+++ index.php
@@ -1,10 +1,5 @@
 <?php
 require_once 'Main.php';
-require_once 'CommissionEmployee.php';
-require_once 'Employee.php';
-require_once 'HourlyEmployee.php';
-require_once 'Person.php';
-require_once 'PieceWorker.php';
 
 
 $entry = new Main();
